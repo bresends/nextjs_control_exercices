c578926136bc3b3dd918810bd301ec29d61844e3
bruno <bruno.resendesantos45@gmail.com>
2022-09-20 14:27:20 -0300

    Feat: Buttons animation added

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className="flex justify-center items-center py-6 space-x-5 md:justify-start md:py-8 md:px-6">
             <Image src="/Vector.svg" alt="My Logo" width={48} height={48} />
-            <h1 className="text-2xl text-white font-bold md:text-5xl">
+            <h1 className="text-2xl text-white font-bold md:text-4xl">
                 Controle de Questões
             </h1>
         </header>
